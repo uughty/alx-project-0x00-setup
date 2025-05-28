32f549ffe3cf99fdb7d6ee36a1748f49b8506d53
Malilo <sandramalilo21@gmail.com>
2025-05-28 16:11:08 +0300

    Added Button.tsx,made changes in index.tsx and landing.tsx

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,9 @@
 export interface PillProps {
-  title: string
+  title: string;
+}
+
+export interface ButtonProps {
+  title: string;
+  size?: "small" | "medium" | "large"; // ✅ Corrected
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full"; // ✅ Corrected
 }
