1600c80341fb3e7bb22d29f423505bd2bcc54e6e
Malilo <sandramalilo21@gmail.com>
2025-05-28 14:40:11 +0300

    Added landing.tsx and About.tsx

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC = () =>{
+    return(
+        <div>
+            <h1 className="text-ul font-extralight">Landing Page</h1>
+        </div>
+    )
+}
+
+export default Landing
