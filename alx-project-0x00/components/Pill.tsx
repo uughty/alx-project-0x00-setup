49b08676a5879bb412dae3e0f4c680b6ec688d6e
Malilo <sandramalilo21@gmail.com>
2025-05-28 15:20:03 +0300

    Added import { PillProps } from @/interfaces; in Pill.tsx

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { PillProps } from "@/interfaces";
 
-interface PillProps {
-  title: string;
-}
+
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">{title}</p>
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
     </div>
-  );
-};
+  )
+}
 
 export default Pill;
